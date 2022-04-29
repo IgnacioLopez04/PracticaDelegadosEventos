eff5ab2589ac2fce3387432151da71f04da98c67
IgnacioLopez04 <ignacioelopez04@gmail.com>
2022-04-29 18:23:16 -0300

    No prometo nada

---
+++ EjerciciosDelegadosEventos/Ejercicio2/Ejercicio2/CajaAhorro.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio2
+{
+    public class CajaAhorro
+    {
+        public string NombreCliente { get; set; }
+        public decimal SaldoActual { get; set; }
+
+        public bool Retirar(decimal cantidadRetiro)
+        {
+            if(cantidadRetiro <= SaldoActual)
+            {
+                SaldoActual -= cantidadRetiro;
+                return true;
+            }
+            return false;
+        }
+
+        public void Depositar(decimal deposito)
+        {
+            SaldoActual += deposito;
+        }
+    }
+}
