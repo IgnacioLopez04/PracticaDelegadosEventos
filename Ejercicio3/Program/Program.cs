353eaf5565e5c73f815d004a126bca1e60a92287
IgnacioLopez04 <ignacioelopez04@gmail.com>
2022-05-02 20:02:37 -0300

    Falta ejercicio 1

---
+++ Ejercicio3/Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Timers;
+using System.Threading.Tasks;
+
+System.Timers.Timer timer = new System.Timers.Timer(1000);
+timer.Elapsed += TimerHandler;
+timer.Enabled = true;
+Console.ReadKey();
+
+static void TimerHandler(object sender, System.Timers.ElapsedEventArgs e)
+{
+    Console.WriteLine(e.SignalTime);
+}
---
+++ EjerciciosDelegadosEventos/PracticaDelegadosEventos/Ejercicio1/CalculoLetras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class CalculoLetras
+    {
+        public int CalcularLetrasNombre(string nombreNuevo, string nombreViejo)
+        {
+            return nombreNuevo.Length - nombreViejo.Length;
+        }
+
+        public void ImprimirTotalLetrasHandler(object? sender, NombreModificadoClase e)
+        {
+            Console.WriteLine($"La cantidad de la diferencia de letras que hay de entre los nombres es: {CalcularLetrasNombre(e.NombreNuevo, e.NombreViejo)}");
+        }
+    }
+}
